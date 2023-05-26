39716883a98e7ddac00a64db04e873dd13c83516
srav001 <sravansuresh1x@gmail.com>
2023-05-26 18:24:56 +0530

    feat: add item card

---
+++ components/ItemCard.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineProps<{
+	item: {
+		id: number;
+		title: string;
+		price: number;
+		category: string;
+		image: string;
+	};
+}>();
+</script>
+
+<template>
+	<div class="item mt-half">
+		<img class="item-image" :src="item.image" :alt="item.title" width="180" height="250" />
+		<div>
+			<p>{{ item.title }}</p>
+			<p style="color: gray">${{ item.price }}</p>
+		</div>
+		<button class="add-item">Add to cart</button>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.item {
+	padding: 2rem;
+	display: flex;
+	margin-top: 0;
+	align-items: center;
+	flex-direction: column;
+	border-radius: 0.7rem;
+	border: 1px solid rgb(128, 128, 128, 0.2);
+}
+
+.add-item {
+	border: 0;
+	border-radius: 0.7rem;
+	background-color: rgb(220, 220, 220, 0.3);
+	padding: 1rem 4rem;
+	font-weight: 600;
+	transition: all 300ms cubic-bezier(0.4, 0, 0.2, 1);
+
+	&:hover {
+		filter: drop-shadow(0 20px 13px rgb(0 0 0 / 0.03)) drop-shadow(0 8px 5px rgb(0 0 0 / 0.08));
+		background-color: black;
+		color: white;
+		cursor: pointer;
+	}
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -36,10 +36,15 @@ export default {
 		<HeaderBar />
 		<section id="banner">Welcome to Voxel Store!</section>
 		<SectionSwitcher :categories="categories" @choose-category="filterByCategory($event)" />
+		<section id="category-section" class="px-common">
+			<TransitionGroup name="cart-items" tag="div" class="grid">
+				<ItemCard v-for="item in postsDisplayed" :key="item.id" :item="item"></ItemCard>
+			</TransitionGroup>
+		</section>
 	</main>
 </template>
 
-<style scoped>
+<style>
 #banner {
 	height: 23rem;
 	background-color: rgb(128, 128, 128, 0.2);
@@ -47,5 +52,24 @@ export default {
 	display: flex;
 	align-items: center;
 	justify-content: center;
+}
+
+.grid {
+	display: grid;
+	grid-template-columns: repeat(4, minmax(0, 1fr));
+	gap: 0.75rem;
+}
+
+.cart-items-move,
+.cart-items-enter-active,
+.cart-items-leave-active {
+	transition: all 300ms cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.cart-items-enter,
+.cart-items-enter-from,
+.cart-items-leave-to {
+	transform: translateX(20px);
+	opacity: 0;
 }
 </style>
